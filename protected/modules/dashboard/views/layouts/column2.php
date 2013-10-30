254e217b10510511eb5997f4a207dca86e00ea0b
Dyego Nery <dyegonr@gmail.com>
2013-10-30 17:57:57 -0200

    Inclusao de coluna de operacoes em views de Dashboard

--- protected/modules/dashboard/views/layouts/column2.php
+++ protected/modules/dashboard/views/layouts/column2.php
@@ -11,9 +11,24 @@
 		</ul>
 	</div><!-- sidebar -->
 </div>
-<div class="col-md-10">
+<div class="col-md-6">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
+</div>
+
+<div class="col-md-4">
+	<div id="sidebar">
+	<?php
+		$this->beginWidget('zii.widgets.CPortlet', array(
+			'title'=>'Operations',
+		));
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+			'htmlOptions'=>array('class'=>'operations'),
+		));
+		$this->endWidget();
+	?>
+	</div><!-- sidebar -->
 </div>
 <?php $this->endContent(); ?>
--- protected/modules/dashboard/views/publications/index.php
+++ protected/modules/dashboard/views/publications/index.php
@@ -2,11 +2,6 @@
 $this->breadcrumbs=array(
 	'Publications',
 );
-
-$this->menu=array(
-	array('label'=>'Create Publications','url'=>array('create')),
-	array('label'=>'Manage Publications','url'=>array('admin')),
-);
 ?>
 
 <h1>Publications</h1>
@@ -15,3 +10,10 @@ $this->menu=array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
 )); ?>
+
+<?php
+	$this->menu=array(
+	array('label'=>'Create Publications','url'=>array('create')),
+	array('label'=>'Manage Publications','url'=>array('admin')),
+);
+?>
